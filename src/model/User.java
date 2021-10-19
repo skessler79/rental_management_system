65fa4e3079604ce696131130e194d76af154ea13
selwyn <selwyn.kessler79@gmail.com>
2021-10-19 13:54:56 +0800

    Project initial structure

---
+++ src/model/User.java
@@ -0,0 +1,13 @@
+package model;
+
+public class User
+{
+    private String username;
+    private String fullName;
+
+    public User(String username, String fullName)
+    {
+        this.username = username;
+        this.fullName = fullName;
+    }
+}
