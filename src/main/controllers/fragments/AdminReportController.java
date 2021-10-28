2470b99cf6d2e929ece3adcd8b5d1565217e8a9c
DN <aqelaffendi@gmail.com>
2021-10-29 00:11:58 +0800

    AR controller

--- src/main/controllers/fragments/AdminReportController.java
+++ src/main/controllers/fragments/AdminReportController.java
@@ -5,6 +5,7 @@ import javafx.beans.binding.BooleanExpression;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -12,8 +13,10 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.Callback;
 import main.classes.Comment;
 import main.classes.properties.Property;
+import main.classes.users.Owner;
 import main.enums.FacilityType;
 import main.enums.PropertyType;
 import main.models.LoginModel;
@@ -41,13 +44,13 @@ public class AdminReportController implements Initializable {
     private TableColumn<Property, PropertyType> ar_col_propertyType;
 
     @FXML
-    private TableColumn<Property, String> ar_col_ownerId;
+    private TableColumn<Property, String> ar_col_ownerName;
 
     @FXML
     private TableColumn<Property, Boolean> ar_col_isActive;
 
     @FXML
-    private TableColumn<Property, ArrayList<Comment>> ar_col_comments;
+    private TableColumn<Property, Integer> ar_col_comments;
 
     @FXML
     private TableColumn<Property, ArrayList<FacilityType>> ar_col_facilityTypes;
@@ -56,29 +59,42 @@ public class AdminReportController implements Initializable {
     public void initialize(URL url, ResourceBundle resourceBundle) {
         PropertyDataModel propertyData = new PropertyDataModel();
         ObservableList<Property> properties = FXCollections.observableArrayList(propertyData.getPropertiesData());
-//        boolean isActive =
 
         ar_table.getColumns().clear();
         ar_col_propertyID.setCellValueFactory(new PropertyValueFactory<Property, String>("propertyId"));
-//        ar_col_propertyID.setCellValueFactory(c -> {
+        ar_col_propertyType.setCellValueFactory(new PropertyValueFactory<Property, PropertyType>("propertyType"));
+        //ar_col_ownerName.setCellValueFactory(new PropertyValueFactory<Owner, String>("username"));
+        ar_col_isActive.setCellValueFactory(new PropertyValueFactory<Property, Boolean>("isActive"));
+        ar_col_facilityTypes.setCellValueFactory(new PropertyValueFactory<Property, ArrayList<FacilityType>>("facilityTypes"));
+        ar_col_comments.setCellValueFactory(new PropertyValueFactory<Property, Integer>("commentCount"));
+
+//        ar_col_ownerName.setCellValueFactory(c -> {
 //            StringProperty var = new SimpleStringProperty((String) properties.get(0).getPropertyId());
 //            return var;
 //        });
 
-        ar_col_propertyType.setCellValueFactory(new PropertyValueFactory<Property, PropertyType>("propertyType"));
-        ar_col_ownerId.setCellValueFactory(new PropertyValueFactory<Property, String>("ownerId"));
-        ar_col_isActive.setCellValueFactory(new PropertyValueFactory<Property, Boolean>("isActive"));
-        ar_col_facilityTypes.setCellValueFactory(new PropertyValueFactory<Property, ArrayList<FacilityType>>("facilityTypes"));
-        ar_col_comments.setCellValueFactory(new PropertyValueFactory<Property, ArrayList<Comment>>("comment"));
+//        TableColumn<RelationManager, String> firstNameColumn = new TableColumn<>("First Name");
+//        firstNameColumn.setCellValueFactory(new Callback<CellDataFeatures<RelationManager,String>, ObservableValue<String>>() {
+//            @Override
+//            public ObservableValue<String> call(CellDataFeatures<RelationManager, String> data) {
+//                return data.getValue() // the RelationManager
+//                        .getPatient().firstNameProperty();
+//            }
+//        });
+
 
         for (Property property:properties) {
-
 //            System.out.println("Test:"+ property);
             ar_table.getItems().add(property);
         }
-
+// change ownerid to ownername, add/delete properties, sort properties according to propertType (choice box), owner (textfield), active/inactive (radio button)
+// display enum values of propertyTypes and facilityTypes
 
         // Comment
-        ar_table.getColumns().addAll(ar_col_propertyID, ar_col_propertyType, ar_col_ownerId, ar_col_isActive, ar_col_facilityTypes, ar_col_comments);
+        ar_table.getColumns().addAll(ar_col_propertyID, ar_col_propertyType, ar_col_ownerName, ar_col_isActive, ar_col_facilityTypes, ar_col_comments);
+    }
+
+    public void deleteButtonClicked() {
+
     }
 }
