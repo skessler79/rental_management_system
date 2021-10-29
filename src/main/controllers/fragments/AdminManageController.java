c8ce3a1d132f206fdd39c035ecdd592b58f93e40
fiyn <alienafis@gmail.com>
2021-10-29 14:29:03 +0800

    update some stuff into admin manage2

--- src/main/controllers/fragments/AdminManageController.java
+++ src/main/controllers/fragments/AdminManageController.java
@@ -12,11 +12,18 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
+import main.classes.properties.Property;
 import main.classes.users.User;
+import main.enums.FacilityType;
+import main.enums.PropertyType;
+import main.models.PropertyDataModel;
 import main.models.UserDataModel;
 import java.util.Collections;
 
@@ -30,12 +37,11 @@ import java.util.ResourceBundle;
 
 public class AdminManageController extends FragmentController implements Initializable {
 
-    UserDataModel userDataModel = new UserDataModel();
+    private UserDataModel userDataModel;
 
-    ArrayList<User> pendinguserlist, userlist;
-    ArrayList<String> usernamelist, pendingusernamelist, emaillist,pendingemailist;
-    ObservableList<String> username_info, email_info;
-
+//    ArrayList<User> pendinguserlist, userlist;
+//    ArrayList<String> usernamelist, pendingusernamelist, emaillist,pendingemailist;
+//    ObservableList<String> username_info, email_info;
 
     @FXML
     private Label pending_users_count;
@@ -50,13 +56,10 @@ public class AdminManageController extends FragmentController implements Initial
     private JFXButton btn_totalUsers;
 
     @FXML
-    private AnchorPane btm_anchorpane1;
+    private AnchorPane anchorpane_total_user;
 
     @FXML
-    private JFXListView<String> listview_total_user;
-
-    @FXML
-    private AnchorPane btm_anchorpane;
+    private AnchorPane anchorpane_pending_user;
 
     @FXML
     private JFXButton btn_reject;
@@ -65,147 +68,195 @@ public class AdminManageController extends FragmentController implements Initial
     private JFXButton btn_accept;
 
     @FXML
-    private TextField txt_usernameAM1;
-
-    @FXML
-    private TextField txt_emailAM1;
-
-    @FXML
     private JFXListView<String> listview_pending_user;
+
+    @FXML
+    private TableView<User> tableview_total_user;
+
+    @FXML
+    private TableColumn<User, String> col_total_user_username;
+
+    @FXML
+    private TableColumn<User, String> col_total_user_email;
+
+    @FXML
+    private TableColumn<User, String> col_total_user_userID;
 
     @FXML
     private JFXButton btn_remove;
 
     @FXML
-    private TextField txt_usernameAM;
+    private TableView<User> tableview_pending_user;
 
     @FXML
-    private TextField txt_emailAM;
+    private TableColumn<User, String> col_pending_user_username;
+
+    @FXML
+    private TableColumn<User, String> col_pending_user_email;
 
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        usernamelist = new ArrayList<>();
-        pendingusernamelist = new ArrayList<>();
-
-        btn_pendingUsers.setOnAction(actionEvent ->  btm_anchorpane1.toFront());
-
-        btn_totalUsers.setOnAction(actionEvent -> btm_anchorpane.toFront());
-
-        btn_reject.setOnAction(actionEvent -> {
-            rejectPendingUser();
-        });
+//        usernamelist = new ArrayList<>();
+//        pendingusernamelist = new ArrayList<>();
 
         setPendingUserNum ();
         setTotalUserNum();
 
-        loadPendingUsersData();
-        loadTotalUsersData();
+        loadTableViewPendingUser();
+        loadTableViewTotalUser();
+
+        btn_pendingUsers.setOnAction(actionEvent ->  anchorpane_pending_user.toFront());
+
+        btn_totalUsers.setOnAction(actionEvent -> anchorpane_total_user.toFront());
+
+        btn_reject.setOnAction(actionEvent -> rejectPendingUser());
+        btn_accept.setOnAction(actionEvent -> acceptPendingUser());
+
+        btn_remove.setOnAction(actionEvent -> removeUser());
 
     }
 
 
-    public void loadPendingUsersData () {
-
-        pendinguserlist = userDataModel.getPendingUserData();
-
-        for(User user: pendinguserlist) {
-            pendingusernamelist.add(user.getUsername());
-        }
-
+//    public void loadPendingUsersData () {
+//
+//        pendinguserlist = userDataModel.getPendingUserData();
+//
+//        for(User user: pendinguserlist) {
+//            pendingusernamelist.add(user.getUsername());
+//        }
+//
 //        for(User user: pendinguserlist) {
 //            pendingemailist.add(user.getEmail());
 //        }
-
-        ObservableList<String> pendinguserlist_observer = FXCollections.observableArrayList(pendingusernamelist);
-        listview_pending_user.getItems().addAll(pendingusernamelist);
-
-        listview_pending_user.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                username_info = listview_pending_user.getSelectionModel().getSelectedItems();
-                email_info = listview_pending_user.getSelectionModel().getSelectedItems();
-
-                txt_usernameAM1.setText(String.valueOf(username_info));
-                txt_emailAM1.setText(String.valueOf(email_info));
-
-            }
-        });
-
-
 //
-//        listview_pending_user.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<User>() {
+//        ObservableList<String> pendinguserlist_observer = FXCollections.observableArrayList(pendingusernamelist);
+//        listview_pending_user.getItems().addAll(pendingusernamelist);
+//
+//        listview_pending_user.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
 //            @Override
-//            public void changed(ObservableValue<? extends User> observableValue, User user, User t1) {
-//                username_info = listview_pending_user.getSelectionModel().getSelectedItems().toString();
-//                email_info = listview_pending_user.getSelectionModel().getSelectedItems().toString();
-//                txt_usernameAM.setText(username_info);
-//                txt_emailAM.setText(email_info);
+//            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+//                username_info = listview_pending_user.getSelectionModel().getSelectedItems();
+//                email_info = listview_pending_user.getSelectionModel().getSelectedItems();
+//
+//                txt_usernameAM1.setText(String.valueOf(username_info));
+//                txt_emailAM1.setText(String.valueOf(email_info));
+//
 //            }
 //        });
+//    }
+
+//    public void loadTotalUsersData () {
+//        userlist = userDataModel.getUserData();
+//
+//        for(User user: userlist) {
+//            usernamelist.add(user.getUsername());
+//        }
+//
+//        for(User user: userlist) {
+//            emaillist.add(user.getEmail());
+//        }
+//
+//        ObservableList<String> usernamelist_observer, emaillist_observer;
+//                //FXCollections.observableArrayList(usernamelist);
+//        listview_total_user.getItems().addAll(usernamelist);
+//
+//        listview_total_user.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+//            @Override
+//            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
+//                username_info = listview_total_user.getSelectionModel().getSelectedItems();
+//                email_info = listview_total_user.getSelectionModel().getSelectedItems();
+//
+//                txt_usernameAM.setText(String.valueOf(username_info));
+//                txt_emailAM.setText(String.valueOf(email_info));
+//
+//            }
+//        });
+//    }
+
+    public void loadTableViewPendingUser() {
+        tableview_pending_user.getItems().clear();
+        userDataModel = new UserDataModel();
+        ObservableList<User> pendinguser = FXCollections.observableArrayList(userDataModel.getPendingUserData()); //will change to pending user data
+
+        tableview_pending_user.getColumns().clear();
+        col_pending_user_username.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
+        col_pending_user_email.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
+
+        for (User user:pendinguser) {
+            tableview_pending_user.getItems().add(user);
+        }
+
+        tableview_pending_user.getColumns().addAll(col_total_user_username, col_total_user_email);
     }
 
-    public void loadTotalUsersData () {
-        userlist = userDataModel.getUserData();
+    public void loadTableViewTotalUser() {
+        tableview_total_user.getItems().clear();
+        userDataModel = new UserDataModel();
+        ObservableList<User> users = FXCollections.observableArrayList(userDataModel.getUserData());
 
-        for(User user: userlist) {
-            usernamelist.add(user.getUsername());
+        tableview_total_user.getColumns().clear();
+        col_total_user_username.setCellValueFactory(new PropertyValueFactory<User, String>("username"));
+        col_total_user_email.setCellValueFactory(new PropertyValueFactory<User, String>("email"));
+        col_total_user_userID.setCellValueFactory(new PropertyValueFactory<User, String>("id"));
+
+        for (User user:users) {
+            tableview_total_user.getItems().add(user);
         }
 
-        for(User user: userlist) {
-            emaillist.add(user.getEmail());
-        }
-
-        ObservableList<String> usernamelist_observer, emaillist_observer;
-                //FXCollections.observableArrayList(usernamelist);
-        listview_total_user.getItems().addAll(usernamelist);
-
-        listview_total_user.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
-            @Override
-            public void changed(ObservableValue<? extends String> observableValue, String s, String t1) {
-                username_info = listview_total_user.getSelectionModel().getSelectedItems();
-                email_info = listview_total_user.getSelectionModel().getSelectedItems();
-
-                txt_usernameAM.setText(String.valueOf(username_info));
-                txt_emailAM.setText(String.valueOf(email_info));
-
-            }
-        });
-
-/*
-        listview_pending_user.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<User>() {
-            @Override
-            public void changed(ObservableValue<? extends User> observableValue, User user, User t1) {
-                username_info = listview_pending_user.getSelectionModel().getSelectedItems().toString();
-                email_info = listview_pending_user.getSelectionModel().getSelectedItems().toString();
-                txt_usernameAM.setText(username_info);
-                txt_emailAM.setText(email_info);
-            }
-        }); */
+        tableview_total_user.getColumns().addAll(col_total_user_userID, col_total_user_username, col_total_user_email);
     }
 
     public void setPendingUserNum () {
+        userDataModel = new UserDataModel();
         int PUcount = userDataModel.getPendingUserData().size();
         String stringPUcount = ("" + PUcount);
         pending_users_count.setText(stringPUcount);
     }
 
     public void setTotalUserNum () {
+        userDataModel = new UserDataModel();
         int TUcount = userDataModel.getUserData().size();
         String stringTUcout = ("" + TUcount);
         total_users_count.setText(stringTUcout);
     }
 
     public void rejectPendingUser() {
+        ObservableList<User> selectedRows, alluser;
+        alluser = tableview_total_user.getItems();
 
+        selectedRows = tableview_total_user.getSelectionModel().getSelectedItems();
+
+        for (User user: selectedRows) {
+            alluser.removeAll(selectedRows);
+        }
+
+        // TODO reject pending users
     }
 
     public void acceptPendingUser() {
+        ObservableList<User> selectedRows, alluser;
+        alluser = tableview_total_user.getItems();
 
+        selectedRows = tableview_total_user.getSelectionModel().getSelectedItems();
+
+        for (User user: selectedRows) {
+            alluser.removeAll(selectedRows);
+        }
+        //TODO accept pending users
     }
 
     public void removeUser() {
+        ObservableList<User> selectedRows, alluser;
+        alluser = tableview_total_user.getItems();
 
+        selectedRows = tableview_total_user.getSelectionModel().getSelectedItems();
+
+        for (User user: selectedRows) {
+            alluser.removeAll(selectedRows);
+        }
+
+        // TODO remove users
     }
 }
