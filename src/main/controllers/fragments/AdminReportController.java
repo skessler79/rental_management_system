a88393171695df8b21acc2fb77cc65edc4d3a2d3
DN <aqelaffendi@gmail.com>
2021-10-29 23:56:34 +0800

    Added 2 project and facility filter

--- src/main/controllers/fragments/AdminReportController.java
+++ src/main/controllers/fragments/AdminReportController.java
@@ -15,12 +15,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.util.Callback;
 import main.classes.Comment;
@@ -40,10 +38,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.text.ParseException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.util.*;
 import java.util.stream.Stream;
 
 public class AdminReportController implements Initializable {
@@ -63,6 +58,8 @@ public class AdminReportController implements Initializable {
     private TableColumn<Property, Integer> ar_col_comments;
     @FXML
     private TableColumn<Property, ArrayList<FacilityType>> ar_col_facilityTypes;
+    @FXML
+    private TableColumn<Property, String> ar_col_project;
 
     @FXML
     private ChoiceBox<PropertyType> ar_choiceBox_property;
@@ -70,11 +67,13 @@ public class AdminReportController implements Initializable {
     ObservableList<PropertyType> ar_choiceBox_list = FXCollections.observableArrayList(PropertyType.values());
     @FXML
     private ChoiceBox<Boolean> ar_choiceBox_activity;
+    @FXML
+    private ChoiceBox<String> ar_choiceBox_facility;
 
-    @FXML
-    private JFXButton ar_btn_facilities;
-    @FXML
-    private JFXButton ar_btn_project;
+//    @FXML
+//    private JFXButton ar_btn_facilities;
+//    @FXML
+//    private JFXButton ar_btn_project;
     @FXML
     private JFXButton ar_btn_del;
     @FXML
@@ -83,7 +82,9 @@ public class AdminReportController implements Initializable {
     private JFXButton ar_btn_clear;
 
     @FXML
-    private JFXTextField ar_textField;
+    private JFXTextField ar_textField_user;
+    @FXML
+    private JFXTextField ar_textField_project;
 
     private PropertyDataModel propertyDataModel;
 
@@ -94,8 +95,6 @@ public class AdminReportController implements Initializable {
         ar_choiceBox_property.setItems(ar_choiceBox_list);
         ar_choiceBox_activity.getItems().addAll(Boolean.TRUE, Boolean.FALSE);
         getTable(CurrentSession.propertyDataModel.getPropertiesData());
-
-// display enum values of propertyTypes and facilityTypes
 
         // Button for deleting a property
         ar_btn_del.setOnAction(e -> {
@@ -108,12 +107,12 @@ public class AdminReportController implements Initializable {
         // Button for filtering by propertyType, owner username, active/inactive
         ar_btn_filter.setOnAction(e -> {
             ArrayList<Property> list;
-            if (ar_textField.getText().trim().isEmpty()) {
+            if (ar_textField_user.getText().trim().isEmpty()) {
                 list = CurrentSession.propertyDataModel.filterProperty(ar_choiceBox_property.getValue(), null, ar_choiceBox_activity.getValue());
                 getTable(list);
             }
             else {
-                list = CurrentSession.propertyDataModel.filterProperty(ar_choiceBox_property.getValue(), CurrentSession.userDataModel.getUserByUsername(ar_textField.getText().trim()), ar_choiceBox_activity.getValue());
+                list = CurrentSession.propertyDataModel.filterProperty(ar_choiceBox_property.getValue(), CurrentSession.userDataModel.getUserByUsername(ar_textField_user.getText().trim()), ar_choiceBox_activity.getValue());
                 getTable(list);
             }
         });
@@ -121,19 +120,38 @@ public class AdminReportController implements Initializable {
         // Clears the filters
         ar_btn_clear.setOnAction(e -> {
             getTable(CurrentSession.propertyDataModel.getPropertiesData());
-            ar_textField.clear();
+            ar_textField_user.clear();
             ar_choiceBox_activity.valueProperty().set(null);
             ar_choiceBox_property.valueProperty().set(null);
         });
 
-        // Shows a list of Facilities in a new window
-        ar_btn_facilities.setOnAction(e -> {
 
-        });
 
-        ar_btn_project.setOnAction(e -> {
-
-        });
+        // For displaying facilityTypes and Projects into a new window
+//        Stage stage = new Stage();
+//        stage.setWidth(250);
+//
+//        ListView listView = new ListView();
+//
+//        // Shows a list of Facilities in a new window
+//        ar_btn_facilities.setOnAction(e -> {
+//            List<FacilityType> list = Arrays.asList(FacilityType.values());
+//
+//            for (FacilityType facilityType: list) {
+//                listView.getItems().add(facilityType);
+//            }
+//
+//            VBox vBox = new VBox();
+//            vBox.getChildren().add(listView);
+//            final Scene scene = new Scene(vBox);
+//            stage.setTitle("Facilities");
+//            stage.setScene(scene);
+//            stage.show();
+//        });
+//
+//        ar_btn_project.setOnAction(e -> {
+//
+//        });
     }
 
     // Deletes the selected property in the table
@@ -157,11 +175,13 @@ public class AdminReportController implements Initializable {
         properties = FXCollections.observableArrayList(propertiesList);
 
         ar_table.getColumns().clear();
+
         ar_col_propertyID.setCellValueFactory(new PropertyValueFactory<Property, String>("propertyId"));
         ar_col_propertyType.setCellValueFactory(new PropertyValueFactory<Property, PropertyType>("propertyType"));
         ar_col_isActive.setCellValueFactory(new PropertyValueFactory<Property, Boolean>("isActive"));
         ar_col_facilityTypes.setCellValueFactory(new PropertyValueFactory<Property, ArrayList<FacilityType>>("facilityTypes"));
         ar_col_comments.setCellValueFactory(new PropertyValueFactory<Property, Integer>("commentCount"));
+        ar_col_project.setCellValueFactory(new PropertyValueFactory<Property, String>("project"));
 
         ar_col_ownerName.setCellValueFactory(c -> {
             StringProperty var = new SimpleStringProperty((String) c.getValue().getOwner().getUsername());
@@ -171,6 +191,6 @@ public class AdminReportController implements Initializable {
         for (Property property:properties) {
             ar_table.getItems().add(property);
         }
-        ar_table.getColumns().addAll(ar_col_propertyID, ar_col_propertyType, ar_col_ownerName, ar_col_isActive, ar_col_facilityTypes, ar_col_comments);
+        ar_table.getColumns().addAll(ar_col_propertyID, ar_col_project, ar_col_propertyType, ar_col_ownerName, ar_col_isActive, ar_col_facilityTypes, ar_col_comments);
     }
 }
